9e5e09a5b10ccf41d75e50889f3c999e72fa13d8
lexaxa <alexoheah@gmail.com>
2018-03-06 00:25:13 +0300

    Add camera

--- app/src/main/java/ru/lavue/audioguide/CameraPreviewActivity.java
+++ app/src/main/java/ru/lavue/audioguide/CameraPreviewActivity.java
@@ -35,19 +35,19 @@ public class CameraPreviewActivity extends AppCompatActivity {
             mCamera = getCameraInstance();
 
 // set Camera parameters
-            Camera.Parameters params = mCamera.getParameters();
-
-            if (params.getMaxNumMeteringAreas() > 0){ // check that metering areas are supported
-                List<Camera.Area> meteringAreas = new ArrayList<>();
-
-                Rect areaRect1 = new Rect(-100, -100, 100, 100);    // specify an area in center of image
-                meteringAreas.add(new Camera.Area(areaRect1, 600)); // set weight to 60%
-                Rect areaRect2 = new Rect(800, -1000, 1000, -800);  // specify an area in upper right of image
-                meteringAreas.add(new Camera.Area(areaRect2, 400)); // set weight to 40%
-                params.setMeteringAreas(meteringAreas);
-            }
-
-            mCamera.setParameters(params);
+//            Camera.Parameters params = mCamera.getParameters();
+//
+//            if (params.getMaxNumMeteringAreas() > 0){ // check that metering areas are supported
+//                List<Camera.Area> meteringAreas = new ArrayList<>();
+//
+//                Rect areaRect1 = new Rect(-100, -100, 100, 100);    // specify an area in center of image
+//                meteringAreas.add(new Camera.Area(areaRect1, 600)); // set weight to 60%
+//                Rect areaRect2 = new Rect(800, -1000, 1000, -800);  // specify an area in upper right of image
+//                meteringAreas.add(new Camera.Area(areaRect2, 400)); // set weight to 40%
+//                params.setMeteringAreas(meteringAreas);
+//            }
+//
+//            mCamera.setParameters(params);
 
             // Create our Preview view and set it as the content of our activity.
             mPreview = new CameraPreview(this, mCamera);
